491ee1b4ef2ee229b4070042aa83090e0ec0bab9
17med <ahmedbenachour2003@gmail.com>
2024-09-19 16:11:21 +0100

    update

--- desktop app/electron/main.ts
+++ desktop app/electron/main.ts
@@ -6,15 +6,6 @@ import path from 'node:path'
 const require = createRequire(import.meta.url)
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
-// The built directory structure
-//
-// ├─┬─┬ dist
-// │ │ └── index.html
-// │ │
-// │ ├─┬ dist-electron
-// │ │ ├── main.js
-// │ │ └── preload.mjs
-// │
 process.env.APP_ROOT = path.join(__dirname, '..')
 
 // 🚧 Use ['ENV_NAME'] avoid vite:define plugin - Vite@2.x
